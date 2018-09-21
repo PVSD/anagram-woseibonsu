315a9c0e0a664f652fa3aaa1407f96412dc2179a
wo580 <winstonobonsu@gmail.com>
2018-09-21 11:22:51 -0400

    T͏̛̯̩̘̰ͅH̵̲̯̗͞I͏̘̤̖̗̤̫̮͟S̬͈ ̵̢\҉͔̯̦̞ͅŞ̶̛̰ ̪͎̦N̴̮̤̥͜ͅO͏҉̝T̷̹̜͙̭̺̣̀̕A̶̹̫̞͠N̩̗̱̲̹͡ ̨̧AR҉͖̼ͅĄ̵̼M͍̲͔͍̞̺͜

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Anagram Detector");
+        System.out.println("Insert your first word:");
+        Scanner kbinput = new Scanner(System.in);
+        String firstWord = kbinput.next();
+        System.out.println("Insert your second word:");
+        kbinput = new Scanner(System.in);
+        String  secondWord = kbinput.next();
+
+        char[] firstArray = firstWord.toLowerCase().toCharArray();
+        char[] secondArray = secondWord.toLowerCase().toCharArray();
+
+        Arrays.sort(firstArray);
+        Arrays.sort(secondArray);
+
+        if(Arrays.equals(firstArray, secondArray))
+        {
+            System.out.println("This is an anagram!");
+        }
+        else
+        {
+            System.out.println("This is not an anagram....");
+        }
+
+
+
+
     }
 }
